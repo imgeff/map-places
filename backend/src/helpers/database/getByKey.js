6054c68af1a46abc2fb80ff63fad0fd804ba62bd
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-10 11:05:01 -0300

    :recycle: refactor: Change getByKey to encompass model place

--- backend/src/helpers/database/getByKey.js
+++ backend/src/helpers/database/getByKey.js
@@ -1,8 +1,8 @@
 const models = require('../../database/models');
 
-async function getByKey(key) {
-  const user = await models.user.findOne({ where: key, raw: true });
-  return user;
+async function getByKey(key, model = 'user') {
+  const entity = await models[model].findOne({ where: key, raw: true });
+  return entity;
 }
 
 module.exports = { getByKey };
