cfa60fd60f61c3911315283681352e2a469c97c1
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-10 13:23:39 -0300

    :sparkles: feat: Add endoint DELETE /place/destroy/:id
    responsible for destroying place

--- backend/src/routes/place.route.js
+++ backend/src/routes/place.route.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const {
   validateNewPlace,
   validateUpdatePlace,
+  validateDestroyPlace,
 } = require('../middlewares/validation/place.validate');
 const placeController = require('../controllers/place.controller');
 const { auth } = require('../middlewares/auth');
@@ -13,5 +14,7 @@ route.post('/create', auth, validateNewPlace, placeController.create);
 route.get('/all', auth, placeController.read);
 
 route.put('/update', auth, validateUpdatePlace, placeController.update);
+
+route.delete('/destroy/:id', auth, validateDestroyPlace, placeController.destroy);
 
 module.exports = route;
