03f34b9a5acf1336f4c0e7bf79d0e8fa7a108ffa
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-11-14 15:01:46 -0300

    :wrench: chore: Update database config file Sequelize

--- backend/src/database/config/config.js
+++ backend/src/database/config/config.js
@@ -1,25 +1,16 @@
-require('dotenv/config');
+require('dotenv').config();
+
+const config = {
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  dialect: 'mysql',
+};
 
 module.exports = {
-  development: {
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-  },
-  test: {
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-  },
-  production: {
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-  },
+  development: config,
+  test: config,
+  production: config,
 };
